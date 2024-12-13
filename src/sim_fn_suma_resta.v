efed6a923cf2ff3a40b109c4d0272d88ab48cef7
Kratos <campospablopatricio@gmail.com>
2024-12-13 15:31:37 -0300

    se cambio el nombre a la entrada de selección

--- src/sim_fn_suma_resta.v
+++ src/sim_fn_suma_resta.v
@@ -20,11 +20,11 @@ module sim_fn_suma_resta;
         $dumpfile("fn_suma_resta.vcd");
         $dumpvars(0, sim_fn_suma_resta);
 
-        // Caso 1: Suma (S = 0)
-        a = 32'd15; b = 32'd10; S = 0;  // 15 + 10
+        // Caso 1: Suma (resta = 0)
+        a = 32'd15; b = 32'd10; resta = 0;  // 15 + 10
         #10 
-        // Caso 2: Resta (S = 1)
-        a = 32'd15; b = 32'd10; S = 1;  // 15 - 10
+        // Caso 2: Resta (resta = 1)
+        a = 32'd15; b = 32'd10; resta = 1;  // 15 - 10
         #10 
 
         $finish;
