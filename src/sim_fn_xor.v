1faec4f315b0ae9fce5dee97613d5a2f0f468a3a
Pablo Patricio Campos <campospablopatricio@gmail.com>
2025-10-22 01:18:49 -0300

    tb fn xor

--- src/sim_fn_xor.v
+++ src/sim_fn_xor.v
@@ -1,8 +1,9 @@
 `include "fn_xor.v"
 module sim_fn_xor ;
     integer i;
-    reg [31:0] a,b;
-    wire [31:0] Y;
+    reg [31:0]a;
+    reg [31:0]b;
+    wire [31:0]Y;
     fn_xor dut (
         .Y (Y),
         .a (a),
@@ -12,27 +13,10 @@ module sim_fn_xor ;
     initial begin
         $dumpfile("fn_xor.vcd");
         $dumpvars(0);
-        //Caso 1 (0 y 0)
-        a = 32'b0; b = 32'b0;
-        #10;
-       
-
-        // Caso 2: a = 32'b0, b = 32'b1 (0 y 1)
-        a = 32'b0; b = 32'b1;
-        #10;
-       
-
-        // Caso 3: a = 32'b1, b = 32'b0 (1 y 0)
-        a = 32'b1; b = 32'b0;
-        #10;
-       
-
-        // Caso 4: a = 32'b1, b = 32'b1 (1 y 1)
-        a = 32'b1; b = 32'b1;
-        #10;
-      
-
-        $finish; 
+       a = 32'h0000_0000; b = 32'h0000_0000; #10;
+       a = 32'h0000_0000; b = 32'hFFFF_FFFF; #10;
+       a = 32'hFFFF_FFFF; b = 32'h0000_0000; #10;
+       a = 32'hFFFF_FFFF; b = 32'hFFFF_FFFF; #10;      
         
     end
 endmodule
