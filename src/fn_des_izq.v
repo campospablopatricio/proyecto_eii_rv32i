9c100e50a3e3cd0d6a69dc6e2cde7aacee773479
Pablo Patricio Campos <campospablopatricio@gmail.com>
2025-10-23 11:03:35 -0300

    revision fn_des_izq

--- src/fn_des_izq.v
+++ src/fn_des_izq.v
@@ -2,10 +2,10 @@
 //un valor de 5 bit. Ingresa ceros por la derecha.
 module fn_des_izq (
     input  [31 : 0] a,
-    input  [4 : 0] b,
+    input  [4 : 0]  b,
     output [31 : 0] Y
    
     
 );
-    assign Y = a << b;
+    assign Y = a << b; //desplazamiento a la izquierda
 endmodule
--- src/sim_fn_des_izq.v
+++ src/sim_fn_des_izq.v
@@ -1,8 +1,8 @@
 `include "fn_des_izq.v"
 module sim_fn_des_izq ;
     integer i;
-    reg [31:0] a;
-    reg [4:0] b;
+    reg  [31:0] a;
+    reg  [4:0]  b;
     wire [31:0] Y;
     fn_des_izq dut (
         .Y (Y),
@@ -12,8 +12,8 @@ module sim_fn_des_izq ;
 
     initial begin
         $dumpfile("fn_des_izq.vcd");
-        $dumpvars(0);
-        a =1 ;
+        $dumpvars(0, sim_fn_des_izq);
+        a =1;
      for (i=0;i<20;i = i + 5) begin
             b = i;
             #10;
